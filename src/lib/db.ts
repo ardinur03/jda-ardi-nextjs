a574d95dd021547ed6ce46b3f97dc7300361dca5
Ardi <ardinurinsan03@gmail.com>
2025-07-28 21:17:51 +0700

    change supabase to neon db & fix typo

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,15 +1,3 @@
-import { PrismaClient } from '@prisma/client'
-
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
-
-declare global {
-  var prisma: undefined | ReturnType<typeof prismaClientSingleton>
-}
-
-const db = globalThis.prisma ?? prismaClientSingleton()
-
-export default db
-
-if (process.env.NODE_ENV !== 'production') globalThis.prisma = db
+import { PrismaClient } from "../../prisma/generated";
+const prisma = new PrismaClient();
+export default prisma;
